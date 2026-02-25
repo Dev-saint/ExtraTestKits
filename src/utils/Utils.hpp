7aecc964eebdd727b0f78c63c8dd59370841523f
Stanislav <90268716+Dev-saint@users.noreply.github.com>
2026-02-25 14:58:41 +0700

    Удалено старое решение (не смог загрузить стандартным образом)

--- src/utils/Utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <algorithm>
-
-inline std::string removeQuotes(const std::string& str) {
-    std::string result = str;
-    result.erase(std::remove(result.begin(), result.end(), '"'), result.end());
-    return result;
-}
