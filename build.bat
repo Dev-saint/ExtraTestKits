1c22a055e1c3482db8c03cd735c8be896b5e19e3
Stanislav <90268716+Dev-saint@users.noreply.github.com>
2026-02-25 14:59:20 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: choose generator, override with environment variable if needed
-if "%CMAKE_GENERATOR%"=="" (
-    set "CMAKE_GENERATOR=MinGW Makefiles"
-)
-echo Using CMake generator: %CMAKE_GENERATOR%
-cmake -S . -B build -G "%CMAKE_GENERATOR%"
-cmake --build build
